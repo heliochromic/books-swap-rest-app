a30f35a4f1991bb855124795a51f3f0a6efee7a9
bohdan <howchromium@gmail.com>
2024-05-24 23:15:45 +0300

    catalog cards added

--- frontend/src/Catalog/BookItem/BookItem.jsx
+++ frontend/src/Catalog/BookItem/BookItem.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import '../Catalog.css'
 
-const BookItem = ({ bookItem }) => {
+const BookItem = ({bookItem}) => {
+    const { photo, bookID } = bookItem;
+
+    console.log(photo)
+
     return (
         <div className="bookItemCard">
-
+            <img src={`http://localhost:8000/${photo}`} alt={bookID.name} className="card-img-top"/>
+            <div className="card-body">
+                <h5 className="card-title">{bookID.name}</h5>
+                <p className="card-text">Author: {bookID.author}</p>
+                <p className="card-text">Genre: {bookID.genre}</p>
+                <p className="card-text">Language: {bookID.language}</p>
+            </div>
         </div>
     )
 }
