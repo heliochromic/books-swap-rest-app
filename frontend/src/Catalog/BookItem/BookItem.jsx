1c2bbb3627e50424816db5aabbea35a0dfd779f7
bohdan <howchromium@gmail.com>
2024-05-25 11:10:58 +0300

    fixed bookItemCards

--- frontend/src/Catalog/BookItem/BookItem.jsx
+++ frontend/src/Catalog/BookItem/BookItem.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import '../Catalog.css'
 
-const BookItem = ({bookItem}) => {
-    const { photo, bookID } = bookItem;
-
-    console.log(photo)
+const BookItem = ({key, bookItem}) => {
+    const {itemID, photo, bookID} = bookItem;
 
     return (
         <div className="bookItemCard">
-            <img src={`http://localhost:8000/${photo}`} alt={bookID.name} className="card-img-top"/>
-            <div className="card-body">
-                <h5 className="card-title">{bookID.name}</h5>
-                <p className="card-text">Author: {bookID.author}</p>
-                <p className="card-text">Genre: {bookID.genre}</p>
-                <p className="card-text">Language: {bookID.language}</p>
-            </div>
+            <a href={`http://localhost:8000/api/catalog/${itemID}`}>
+                <div className="imageWrapper">
+                    <img src={`http://localhost:8000/${photo}`} alt={bookID.name} className="card-img-top"/>
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title">{bookID.name}</h5>
+                    <p className="card-text">Author: {bookID.author}</p>
+                    <p className="card-text">Genre: {bookID.genre}</p>
+                    <p className="card-text">Language: {bookID.language}</p>
+                </div>
+            </a>
         </div>
     )
 }
