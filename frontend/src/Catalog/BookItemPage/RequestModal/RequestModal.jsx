521af96c4e0274d696b6414b3f12ac8b6fe8cf14
Volodymyr <volodymyr2126@gmail.com>
2024-05-31 01:55:11 +0300

    kill me

--- frontend/src/Catalog/BookItemPage/RequestModal/RequestModal.jsx
+++ frontend/src/Catalog/BookItemPage/RequestModal/RequestModal.jsx
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import axios from "axios";
 import BookItemTiny from "./BookItemTiny/BookItemTiny";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const RequestModal = () => {
