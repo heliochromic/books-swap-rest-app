d824f9799779b8360c182abe425c8970ec5ba38b
bohdan <howchromium@gmail.com>
2024-06-03 00:11:37 +0300

    styles for catalog fixed

--- frontend/src/Catalog/BookItem/BookItem.jsx
+++ frontend/src/Catalog/BookItem/BookItem.jsx
@@ -9,8 +9,8 @@ const BookItem = ({bookItem}) => {
         <div className="bookItemCard">
             <a href={`http://localhost:3000/catalog/${itemID}`}>
                 <div className="card rounded-5">
-                    <div className="card-img-top p-3 overflow-hidden">
-                        <span className="card-img-fit overflow-hidden"
+                    <div className="card-img-top p-3">
+                        <span className="card-img-fit"
                               style={{
                                   backgroundImage: `url(http://localhost:8000/${photo})`,
                               }}/>
